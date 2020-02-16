e758b2adebb2ae105b761c492e2571d4abcc3111
snpefk <snpefk@gmail.com>
2020-02-17 00:58:18 +0300

    tests: fix & add test

--- tests/test.rs
+++ tests/test.rs
@@ -1,9 +1,16 @@
-extern crate opensi;
-use std::path::Path;
-use std::env;
+use opensi::Package;
+
+const PATH: &str = "tests/data/slamjam2.siq";
 
 #[test]
 fn open_pack() {
-    let package = opensi::open("tests/data/slamjam2.siq");
+    let package = Package::open(PATH);
     assert_eq!(package.is_ok(), true);
+}
+
+#[test]
+fn read_package_name() {
+    let package = Package::open(PATH).expect("pack is not found"); 
+    assert_eq!(package.name.is_some(), true);
+    assert_eq!(package.name.unwrap(), "SLAM JAM 2".to_owned());
 }
