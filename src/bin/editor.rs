224a6b0b7d3ff71aa158c33bd9248cf20e06225a
snpefk <snpefk@gmail.com>
2020-03-12 23:48:32 +0300

    editor: add showing package in TreeView

--- src/bin/editor.rs
+++ src/bin/editor.rs
@@ -1,25 +1,30 @@
+use std::io;
+
 use gtk::prelude::*;
 use gtk::Orientation::Vertical;
-use gtk::{Inhibit, WidgetExt, Window, WindowType};
+use gtk::{
+    prelude::GtkListStoreExtManual, CellLayoutExt, ContainerExt, GtkWindowExt, Inhibit,
+    TreeModelExt, TreeSelectionExt, TreeStore, TreeView, TreeViewExt, WidgetExt, Window,
+    WindowType,
+};
 use relm::{connect, Relm, Update, Widget};
 use relm_derive::Msg;
 
-struct Model {}
+use opensi;
 
 #[derive(Msg)]
 enum Msg {
+    ItemSelect,
     Quit,
 }
 
-// Create the structure that holds the widgets used in the view.
-#[derive(Clone)]
-struct Widgets {
+struct Win {
+    tree_view: TreeView,
+    model: Model,
     window: Window,
 }
-
-struct Win {
-    model: Model,
-    widgets: Widgets,
+struct Model {
+    package: opensi::Package,
 }
 
 impl Update for Win {
@@ -28,11 +33,26 @@ impl Update for Win {
     type Msg = Msg;
 
     fn model(_: &Relm<Self>, _: ()) -> Model {
-        Model {}
+        Model {
+            package: opensi::Package::open("tests/data/slamjam2.siq").expect("cant't open package"),
+        }
     }
 
     fn update(&mut self, event: Msg) {
         match event {
+            Msg::ItemSelect => {
+                let selection = self.tree_view.get_selection();
+                if let Some((list_model, iter)) = selection.get_selected() {
+                    // let round = list_model
+                    //     .get_value(&iter, VALUE_COL)
+                    //     .get::<String>()
+                    //     .ok()
+                    //     .and_then(|value| value)
+                    //     .expect("operation failed");
+
+                    // self.tree_view.set_model(Some(&list_model));
+                }
+            }
             Msg::Quit => gtk::main_quit(),
         }
     }
@@ -42,15 +62,36 @@ impl Widget for Win {
     type Root = Window;
 
     fn root(&self) -> Self::Root {
-        self.widgets.window.clone()
+        self.window.clone()
     }
 
     fn view(relm: &Relm<Self>, model: Self::Model) -> Self {
-        let window = Window::new(WindowType::Toplevel);
-        window.set_title("opensi editor");
-        window.set_property_default_width(640);
-        window.set_property_default_height(480);
+        let window = gtk::Window::new(WindowType::Toplevel);
+        let vbox = gtk::Box::new(Vertical, 0);
+        let tree_view = gtk::TreeView::new();
+        let column = gtk::TreeViewColumn::new();
+        let cell = gtk::CellRendererText::new();
 
+        window.set_title("Package editor");
+        window.set_position(gtk::WindowPosition::Center);
+        window.set_default_size(350, 70);
+
+        column.pack_start(&cell, true);
+        column.add_attribute(&cell, "text", 0);
+        tree_view.append_column(&column);
+
+        let store_model = model
+            .package
+            .to_treestore()
+            .expect("create_and_fill_model failed");
+        tree_view.set_model(Some(&store_model));
+
+        vbox.add(&tree_view);
+        window.add(&vbox);
+
+        window.show_all();
+
+        connect!(relm, tree_view, connect_cursor_changed(_), Msg::ItemSelect);
         connect!(
             relm,
             window,
@@ -58,15 +99,41 @@ impl Widget for Win {
             return (Some(Msg::Quit), Inhibit(false))
         );
 
-        window.show_all();
-
         Win {
+            tree_view,
             model,
-            widgets: Widgets { window: window },
+            window,
         }
+    }
+}
+// These two constants stand for the columns of the listmodel and the listview
+const VALUE_COL: i32 = 0;
+const INDEX_COL: i32 = 1;
+
+impl TreeStorable for opensi::Package {
+    fn to_treestore(&self) -> io::Result<TreeStore> {
+        let store = gtk::TreeStore::new(&[String::static_type(), i32::static_type()]);
+
+        self.rounds.rounds.iter().enumerate().for_each(|(i, round)| {
+            store.insert_with_values(
+                None,
+                None,
+                &[VALUE_COL as u32, INDEX_COL as u32],
+                &[&round.name, &(i as u32)],
+            );
+        });
+
+        println!("{:?}", self);
+        Ok(store)
     }
 }
 
 fn main() {
     Win::run(()).expect("run failed");
+}
+
+/// Intended to convert Package parts to gtk::TreeStore
+trait TreeStorable {
+    // let store = gtk::TreeStore::new(&[String::static_type(), bool::static_type()]);
+    fn to_treestore(&self) -> io::Result<TreeStore>;
 }
