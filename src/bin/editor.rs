a14b3c8d7e730694501b5a7cb952e760c939cb75
snpefk <snpefk@gmail.com>
2020-03-12 23:48:32 +0300

    Editor: add render (incomplete) game package in TreeView

--- src/bin/editor.rs
+++ src/bin/editor.rs
@@ -1,25 +1,29 @@
+use std::io;
+
 use gtk::prelude::*;
 use gtk::Orientation::Vertical;
-use gtk::{Inhibit, WidgetExt, Window, WindowType};
+use gtk::{
+    CellLayoutExt, ContainerExt, GtkWindowExt, Inhibit, TreeStore, TreeView, TreeViewExt,
+    WidgetExt, Window, WindowType,
+};
 use relm::{connect, Relm, Update, Widget};
 use relm_derive::Msg;
 
-struct Model {}
+use opensi;
 
 #[derive(Msg)]
 enum Msg {
+    ItemSelect,
     Quit,
 }
 
-// Create the structure that holds the widgets used in the view.
-#[derive(Clone)]
-struct Widgets {
+struct Win {
+    tree_view: TreeView,
+    model: Model,
     window: Window,
 }
-
-struct Win {
-    model: Model,
-    widgets: Widgets,
+struct Model {
+    package: opensi::Package,
 }
 
 impl Update for Win {
@@ -28,11 +32,20 @@ impl Update for Win {
     type Msg = Msg;
 
     fn model(_: &Relm<Self>, _: ()) -> Model {
-        Model {}
+        Model {
+            // TODO: will be replaced with actual data in future
+            package: opensi::Package::open("tests/data/slamjam2.siq").expect("cant't open package"),
+        }
     }
 
     fn update(&mut self, event: Msg) {
         match event {
+            Msg::ItemSelect => {
+                let selection = self.tree_view.get_selection();
+                if let Some((list_model, iter)) = selection.get_selected() {
+                    // TODO: add action here 
+                }
+            }
             Msg::Quit => gtk::main_quit(),
         }
     }
@@ -42,15 +55,33 @@ impl Widget for Win {
     type Root = Window;
 
     fn root(&self) -> Self::Root {
-        self.widgets.window.clone()
+        self.window.clone()
     }
 
     fn view(relm: &Relm<Self>, model: Self::Model) -> Self {
-        let window = Window::new(WindowType::Toplevel);
-        window.set_title("opensi editor");
-        window.set_property_default_width(640);
-        window.set_property_default_height(480);
+        let window = gtk::Window::new(WindowType::Toplevel);
+        let vbox = gtk::Box::new(Vertical, 0);
+        let tree_view = gtk::TreeView::new();
+        let column = gtk::TreeViewColumn::new();
+        let cell = gtk::CellRendererText::new();
 
+        window.set_title("Package editor");
+        window.set_position(gtk::WindowPosition::Center);
+        window.set_default_size(350, 70);
+
+        column.pack_start(&cell, true);
+        column.add_attribute(&cell, "text", 0);
+        tree_view.append_column(&column);
+
+        let store_model = to_treestore(&model.package).expect("convert to TreeStore failed");
+        tree_view.set_model(Some(&store_model));
+
+        vbox.add(&tree_view);
+        window.add(&vbox);
+
+        window.show_all();
+
+        connect!(relm, tree_view, connect_cursor_changed(_), Msg::ItemSelect);
         connect!(
             relm,
             window,
@@ -58,13 +89,54 @@ impl Widget for Win {
             return (Some(Msg::Quit), Inhibit(false))
         );
 
-        window.show_all();
-
         Win {
+            tree_view,
             model,
-            widgets: Widgets { window: window },
+            window,
         }
     }
+}
+
+// ugly, need refactor
+fn to_treestore(package: &opensi::Package) -> io::Result<TreeStore> {
+    let store = gtk::TreeStore::new(&[String::static_type()]);
+
+    package.rounds.rounds.iter().for_each(|round| {
+        let round_parent = store.insert_with_values(None, None, &[0 as u32], &[&round.name]);
+
+        round.themes.themes.iter().for_each(|theme| {
+            let theme_parent =
+                store.insert_with_values(Some(&round_parent), None, &[0 as u32], &[&theme.name]);
+
+            theme.questions.questions.iter().for_each(|question| {
+                let atom = &question
+                    .scenario
+                    .atoms
+                    .first()
+                    .expect("failed to extract atom");
+                let title = atom.body.as_ref().expect("heh");
+                let title = format!("{} ({})", title, question.price);
+
+                let atom_store =
+                    store.insert_with_values(Some(&theme_parent), None, &[0 as u32], &[&title]);
+
+                let empty = String::from("");
+                let answers = &question
+                    .right
+                    .answers
+                    .iter()
+                    .map(|answer| answer.body.as_ref().unwrap_or(&empty).clone())
+                    .collect::<Vec<String>>()
+                    .join(", ");
+
+                store.insert_with_values(Some(&atom_store), None, &[0 as u32], &[&answers]);
+            })
+        });
+    });
+
+    // add flag for printing only for debug version
+    println!("{:?}", package);
+    Ok(store)
 }
 
 fn main() {
