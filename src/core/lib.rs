8d4e9d00df9f1835c591a9ecf5aa03a536b9c4a5
snpefk <snpefk@gmail.com>
2020-05-17 21:28:12 +0300

    core: add function that open package with extraction resource to /tmp

--- src/core/lib.rs
+++ src/core/lib.rs
@@ -7,7 +7,8 @@ use std::io::ErrorKind;
 use std::path::Path;
 use PartialEq;
 
-use quick_xml::de::{from_str, DeError};
+use percent_encoding::{utf8_percent_encode, AsciiSet, CONTROLS};
+use quick_xml::de::{from_reader, from_str, DeError};
 use serde::Deserialize;
 
 #[derive(Clone, Debug, Deserialize, PartialEq)]
@@ -123,6 +124,8 @@ pub struct Atom {
     pub body: Option<String>,
 }
 
+const FRAGMENT: &AsciiSet = &CONTROLS.add(b' ');
+
 impl Package {
     pub fn open<P: AsRef<Path>>(path: P) -> Result<Package, std::io::Error> {
         let package_file = File::open(path)?;
@@ -137,5 +140,62 @@ impl Package {
     fn parse(xml: &str) -> Result<Package, DeError> {
         let package: Package = from_str(xml)?;
         Result::Ok(package)
+    }
+
+    pub fn open_with_extraction<P: AsRef<Path>>(path: P) -> Result<Package, std::io::Error> {
+        let package_name = path.as_ref().file_name().unwrap().to_str().unwrap();
+        let package_file = File::open(&path)?;
+        let mut zip = zip::ZipArchive::new(package_file)?;
+
+        let xml_path = Self::extract_package(package_name, &mut zip)?;
+        let xml = std::fs::File::open(xml_path)?;
+        let reader = std::io::BufReader::new(xml);
+        from_reader(reader).map_err(|e| std::io::Error::new(ErrorKind::InvalidData, e))
+    }
+
+    /// Extract package internals into `/tmp/{package_name}/`. Return `PathBuf`
+    /// to `content.xml`.
+    ///
+    /// # Arguments
+    /// * `package_name` - package name to which the package will be extracted
+    /// * `zip` - unpacked archive
+    fn extract_package(
+        package_name: &str,
+        zip: &mut zip::ZipArchive<File>,
+    ) -> Result<std::path::PathBuf, std::io::Error> {
+        let tmp = std::env::temp_dir().join(package_name);
+
+        for i in 0..zip.len() {
+            let mut zipfile = zip.by_index(i)?;
+            let relative_file_path = zipfile.sanitized_name();
+            let file_name = &relative_file_path.file_name().unwrap().to_str().unwrap();
+            let encoded_name = utf8_percent_encode(&file_name, FRAGMENT).to_string();
+            let encoded_name: &str = if encoded_name.starts_with("@") {
+                &encoded_name[1..]
+            } else {
+                &encoded_name
+            };
+
+            let absolute_file_path: std::path::PathBuf = if let Some(parent) =
+                relative_file_path.parent()
+            {
+                let mut absolute_parent_path: std::path::PathBuf = [&tmp, parent].iter().collect();
+                if !absolute_parent_path.exists() {
+                    std::fs::create_dir_all(&absolute_parent_path).unwrap();
+                }
+
+                absolute_parent_path.push(encoded_name.to_string());
+                absolute_parent_path
+            } else {
+                let mut absolute_path = std::path::PathBuf::from(&tmp);
+                absolute_path.push(encoded_name.to_string());
+                absolute_path
+            };
+
+            let mut fsfile = std::fs::File::create(&absolute_file_path)?;
+            std::io::copy(&mut zipfile, &mut fsfile)?;
+        }
+        let content = std::path::PathBuf::from("content.xml");
+        Ok([tmp, content].iter().collect())
     }
 }
