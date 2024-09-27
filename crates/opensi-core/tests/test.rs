a76cf8255b046557e2a0d22be199f40bd352bc3a
barsoosayque <shtoshich@gmail.com>
2024-09-27 16:34:16 +0200

    core: fix tests

--- crates/opensi-core/tests/test.rs
+++ crates/opensi-core/tests/test.rs
@@ -7,13 +7,13 @@ const PATH: &str = "tests/data/slamjam2.siq";
 
 #[test]
 fn open_pack() {
-    let package = Package::open(PATH);
+    let package = Package::open_zip_file(PATH);
     assert_eq!(package.is_ok(), true);
 }
 
 #[test]
 fn read_package_name() {
-    let package = Package::open(PATH).expect("pack is not found");
+    let package = Package::open_zip_file(PATH).expect("pack is not found");
     assert_eq!(package.name.is_some(), true);
     assert_eq!(package.name.unwrap(), "SLAM JAM 2".to_owned());
 }
