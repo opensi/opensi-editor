9bbc668926c59141ec49662a40c0a732492a4204
Fedor Tolstonozhenko <fedor.tolstonozhenko@jetbrains.com>
2024-12-23 16:33:45 +0100

    Update test to include more packages

--- crates/opensi-core/tests/check_data.rs
+++ crates/opensi-core/tests/check_data.rs
@@ -1,24 +1,52 @@
 use opensi_core::prelude::*;
 
-const PATH: &str = "tests/data/slamjam2.siq";
+use std::fs;
+use std::path::PathBuf;
+
+const PACKS_DIR: &str = "tests/data";
 
 #[test]
-fn open_pack() {
-    let package = Package::open_zip_file(PATH);
-    assert_eq!(package.is_ok(), true);
-}
+fn open_packs() {
+    for pack in get_packs() {
+        let package = Package::open_zip_file(&pack);
+        let package_name = pack.file_name().unwrap().to_str().unwrap();
 
-#[test]
-fn read_package_name() {
-    let package = Package::open_zip_file(PATH).expect("pack is not found");
-    assert_eq!(package.name, "SLAM JAM 2".to_owned());
+        assert!(package.is_ok(), "Can't open package {}, error is {}", package_name, package.err().unwrap());
+    }
 }
 
 #[test]
 fn resave_test() {
-    let package_original = Package::open_zip_file(PATH).expect("Pack is not found");
-    let bytes = &package_original.to_bytes().expect("Can't serialize package to bytes");
-    let package_resaved = Package::from_zip_buffer(bytes).expect("Can't read package from buffer");
+    for pack in get_packs() {
+        let package_original = Package::open_zip_file(&pack).expect("Pack is not found");
+        let bytes = &package_original.to_bytes().expect("Can't serialize package to bytes");
+        let package_resaved =
+            Package::from_zip_buffer(bytes).expect("Can't read package from buffer");
 
-    assert_eq!(package_original, package_resaved);
+        let package_name = pack.file_name().unwrap().to_str().unwrap();
+
+        assert_eq!(
+            package_original,
+            package_resaved,
+            "Package {} resaving produced different results",
+            package_name
+        );
+    }
+}
+
+fn get_packs() -> Vec<PathBuf> {
+    let mut packs = Vec::new();
+
+    if let Ok(entries) = fs::read_dir(PACKS_DIR) {
+        for entry in entries {
+            if let Ok(entry) = entry {
+                let path = entry.path();
+                if path.is_file() && path.extension().unwrap_or_default() == "siq" {
+                    packs.push(path);
+                }
+            }
+        }
+    }
+
+    packs
 }
