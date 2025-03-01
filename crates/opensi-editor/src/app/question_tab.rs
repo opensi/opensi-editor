c83caab1edf8ad39a2dc0bc9cbf0e8865bc83e9c
barsoosayque <shtoshich@gmail.com>
2025-03-01 21:50:52 +0700

    Editor: add/remove/edit answers

--- crates/opensi-editor/src/app/question_tab.rs
+++ crates/opensi-editor/src/app/question_tab.rs
@@ -1,8 +1,8 @@
 use opensi_core::prelude::*;
 
 use crate::{
-    element::{PropertyTable, Sections, info_edit, unselectable_label},
-    icon, icon_str, icon_string,
+    element::{PropertyTable, Sections, danger_button, info_edit, unselectable_label},
+    icon, icon_str,
 };
 
 pub fn question_tab(question: &mut Question, ui: &mut egui::Ui) {
@@ -48,29 +48,6 @@ fn question_scenario(question: &mut Question, ui: &mut egui::Ui) {
     });
 }
 
-fn question_answers(question: &mut Question, ui: &mut egui::Ui) {
-    ui.with_layout(egui::Layout::top_down_justified(egui::Align::Min), |ui| {
-        for answer in &mut question.right {
-            ui.add(
-                egui::Label::new(
-                    egui::RichText::new(icon_string!(CHECK_FAT, answer))
-                        .color(ui.visuals().widgets.hovered.text_color()),
-                )
-                .selectable(false),
-            );
-        }
-        for answer in &mut question.wrong {
-            ui.add(
-                egui::Label::new(
-                    egui::RichText::new(icon_string!(PLACEHOLDER, answer))
-                        .color(ui.visuals().error_fg_color),
-                )
-                .selectable(false),
-            );
-        }
-    });
-}
-
 fn atom_ui(atom: &mut Atom, ui: &mut egui::Ui) {
     ui.horizontal(|ui| {
         match atom.kind {
@@ -95,5 +72,101 @@ fn atom_ui(atom: &mut Atom, ui: &mut egui::Ui) {
                 unselectable_label(format!("{atom:?}"), ui);
             },
         }
+    });
+}
+
+fn question_answers(question: &mut Question, ui: &mut egui::Ui) {
+    ui.with_layout(egui::Layout::top_down_justified(egui::Align::Min), |ui| {
+        for n in 0..question.right.len() {
+            answer_ui(question, n, false, ui);
+        }
+        for n in 0..question.wrong.len() {
+            answer_ui(question, n, true, ui);
+        }
+
+        ui.horizontal_top(|ui| {
+            if ui.button(icon_str!(CHECK_FAT, "Добавить правильный")).clicked() {
+                question.right.push(format!("Правильный ответ #{}", question.right.len() + 1));
+            }
+
+            if ui.button(icon_str!(PLACEHOLDER, "Добавить неправильный")).clicked()
+            {
+                question.wrong.push(format!("Неправильный ответ #{}", question.wrong.len() + 1));
+            }
+        });
+    });
+}
+
+fn answer_ui(question: &mut Question, n: usize, is_wrong: bool, ui: &mut egui::Ui) {
+    if (is_wrong && n >= question.wrong.len()) || (!is_wrong && n >= question.right.len()) {
+        return;
+    }
+
+    ui.push_id(ui.id().with("answer").with(is_wrong).with(n), |ui| {
+        let edit_id = ui.id().with("edit");
+        let is_edit = ui.memory(|memory| memory.data.get_temp::<bool>(edit_id)).unwrap_or_default();
+
+        let (icon, color) = if is_wrong {
+            (icon!(PLACEHOLDER), ui.visuals().error_fg_color)
+        } else {
+            (icon!(CHECK_FAT), ui.visuals().widgets.hovered.text_color())
+        };
+
+        ui.set_max_height(22.0);
+
+        egui_extras::StripBuilder::new(ui)
+            .size(egui_extras::Size::initial(20.0))
+            .size(egui_extras::Size::remainder())
+            .size(egui_extras::Size::initial(50.0))
+            .cell_layout(
+                egui::Layout::left_to_right(egui::Align::Min).with_main_align(egui::Align::Min),
+            )
+            .horizontal(|mut strip| {
+                strip.cell(|ui| {
+                    ui.add(
+                        egui::Label::new(egui::WidgetText::from(icon).color(color))
+                            .selectable(false),
+                    );
+                });
+
+                strip.cell(|ui| {
+                    let answer =
+                        if is_wrong { &mut question.wrong[n] } else { &mut question.right[n] };
+
+                    if is_edit {
+                        let response =
+                            ui.add(egui::TextEdit::singleline(answer).desired_width(f32::INFINITY));
+                        response.request_focus();
+
+                        if response.has_focus()
+                            && ui.input(|input| input.key_pressed(egui::Key::Enter))
+                        {
+                            ui.memory_mut(|memory| memory.data.insert_temp(edit_id, false));
+                        }
+                    } else {
+                        ui.add(egui::Label::new(egui::RichText::new(answer.as_str()).color(color)));
+                    }
+                });
+
+                strip.cell(|ui| {
+                    if is_edit {
+                        if ui.button(icon!(FLOPPY_DISK)).clicked() {
+                            ui.memory_mut(|memory| memory.data.insert_temp(edit_id, false));
+                        }
+                    } else {
+                        if ui.button(icon!(PENCIL)).clicked() {
+                            ui.memory_mut(|memory| memory.data.insert_temp(edit_id, true));
+                        }
+                    }
+
+                    if danger_button(icon!(TRASH), ui).clicked() {
+                        if is_wrong {
+                            question.wrong.remove(n);
+                        } else {
+                            question.right.remove(n);
+                        }
+                    }
+                });
+            });
     });
 }
