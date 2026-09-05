680fa89196098dee83ddbc4e3e94c34c26d6adcc
barsoosayque <shtoshich@gmail.com>
2026-09-06 03:45:37 +0600

    flake: update 2026/09/06

--- flake.nix
+++ flake.nix
@@ -47,10 +47,10 @@
           wayland
 
           # x11 libraries
-          xorg.libXcursor
-          xorg.libXrandr
-          xorg.libXi
-          xorg.libX11
+          libxcursor
+          libxrandr
+          libxi
+          libx11
         ];
 
         # IDE/shell dependencies
